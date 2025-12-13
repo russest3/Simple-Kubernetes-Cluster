0d8a88f86f25fbb55eaf9532565dcceccd97153a
russest3 <s.p.russell00@gmail.com>
2025-12-13 18:21:37 -0500

    many updates

--- README.md
+++ README.md
@@ -11,6 +11,7 @@
 - Run `aws configure` to configure access keys
 
 ## AWS CDK Procedure
+
 ```
 $ cd workspace
 $ cdk deploy
@@ -29,12 +30,40 @@ SSH to an instance with
 ssh ubuntu@c1-cp1 -i ~/.ssh/<key>.pub
 ```
 
-Setup kubectl locally:
+Install nginx gateway fabric
 
 ```
-kubectl config set-cluster mycluster --server=https://<public_ip>:6443 --certificate-authority=ca.crt
-kubectl config set-credentials ubuntu --client-certificate=~/.ssh/id_rsa.pub --client-key=~/.ssh/id_rsa
-kubectl config set-context current --cluster=mycluster --user=ubuntu
+kubectl kustomize "https://github.com/nginx/nginx-gateway-fabric/config/crd/gateway-api/standard?ref=v2.2.2" | kubectl apply -f -
+
+helm install ngf oci://ghcr.io/nginx/charts/nginx-gateway-fabric --create-namespace -n nginx-gateway --set nginx.service.type=NodePort --set-json 'nginx.service.nodePorts=[{"port":31437,"listenerPort":80}]'
+
+gateway-class.yaml:
+
+apiVersion: gateway.networking.k8s.io/v1
+kind: GatewayClass
+metadata:
+  name: nginx
+  namespace: nginx-gateway
+spec:
+  controllerName: nginx.org/nginx-gateway-controller
+
+http gateway and listener:
+
+apiVersion: gateway.networking.k8s.io/v1
+kind: Gateway
+metadata:
+  name: nginx-gateway
+  namespace: nginx-gateway
+spec:
+  gatewayClassName: nginx
+  listeners:
+  - name: http
+    protocol: HTTP
+    port: 80
+    allowedRoutes:
+      namespaces:
+        from: All
+
 ```
 
 Enjoy!
--- workspace/workspace/workspace_stack.py
+++ workspace/workspace/workspace_stack.py
@@ -11,6 +11,15 @@ class WorkspaceStack(Stack):
 
         ami_id = "ami-0529b091671d10bda"  # Updated to 1.34 ! 12/13
 
+        publicKey = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDCIZGs+NXLVki+CR9u200ehxdSE7XzljFzQXiKLObPZlWdo0MEKuxpCGhVuxqhJjPYhh2zG/AJggRzuLg/FZdxDT+cIITVii11Vxl2Ry/h3DHvJKMptZfOtzCeEoUvoTg2Mj8ets1EMc0WrmG9tSNKh5oCqmGfn2AnLrGUkXfabG9BgRJIPYciDLqVpC8eQ+QZmCQY7r78SIc8c78wsPBqn5j5yjaNbLXIyAAduZgwJ3FUO9j541+0qOQxREIvVgE4M/7sVoPA+emZPc+HOXw8D3U8BQGyn40GaGJM20N9odc2qVHquD1V6O9qjO0aj4L7GZtDBeowjw2tUyKoYbV7tatU/AiQX1DaNitdVoh3t1HQGfshz+Cy7Nh2uFIDeypvwrXfHMVmGeDTnPuCdzKUi3eAT0V4XsJpQLC3jR5CcbX5BVN6lACaxFmhy3nl2adxbKpHrNbdtXhDhCMG8B9xI0WHwKZviQ5LCLPbslz5WdTbKHwHu2p7S76W9taglPYEKMmf8rwvHj9u/5ysmFH0X4YThjvlyvWheLmlxrpQSL9VzrQ7FcBgLU/+beHgDFY6e/pYz22KzGyZrgbwseee1dC1VKwkcTkMuUFcAvUqaQSF9hucts5NwA/ZpGxwslzK5sVaWkZafzkDN7/iYuHS1N3vjUIkE/sA8xaWJvfL4Q== russest3@pop-os"
+
+        keyPair = ec2.KeyPair(self, "KubernetesKeyPair",
+            account="014420964653",
+            key_pair_name="KubernetesKeyPair",
+            region="us-east-2",
+            public_key_material=publicKey,
+        )
+
         vpc = ec2.Vpc(self, "Vpc",
             max_azs=1,
             cidr="10.192.0.0/16",
@@ -24,10 +33,10 @@ class WorkspaceStack(Stack):
             ]
         )
 
-        keyPair = ec2.KeyPair.from_key_pair_attributes(self, "KeyPair",
-                    key_pair_name="KubernetesKeyPair",
-                    type=ec2.KeyPairType.RSA
-                )
+        # keyPair = ec2.KeyPair.from_key_pair_attributes(self, "KeyPair",
+        #             key_pair_name="KubernetesKeyPair",
+        #             type=ec2.KeyPairType.RSA
+        #         )
         
         ec2_role = iam.Role(self, "Ec2Role",
             assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"),
@@ -69,23 +78,17 @@ class WorkspaceStack(Stack):
 
         c1_cp1_user_data = ec2.UserData.for_linux()
         c1_cp1_user_data.add_commands(
-            "hostname c1-cp1",
-            "echo 'c1-cp1 > /etc/hostname'",
+            "hostnamectl hostname c1-cp1",
             "apt update -y",
             "apt upgrade -y",
             "kubeadm init --kubernetes-version v1.34.1 --pod-network-cidr=10.244.0.0/16 --upload-certs --ignore-preflight-errors=NumCPU,Mem",
-            "mkdir -p /home/ubuntu/.kube",
-            "chown ubuntu:ubuntu /home/ubuntu/.kube",
             "cp /etc/kubernetes/admin.conf /home/ubuntu/.kube/config",
             "chown ubuntu:ubuntu /home/ubuntu/.kube/config",
-            "sudo su - ubuntu",
-            "cd /home/ubuntu",
-            "wget https://raw.githubusercontent.com/russest3/aws-vpc-with-client-vpn-endpoint/refs/heads/main/workspace/workspace/kube-flannel.yml",
-            "kubectl apply -f /home/ubuntu/kube-flannel.yml",
-            "wget https://get.helm.sh/helm-v4.0.0-rc.1-linux-amd64.tar.gz",
-            "tar -xvzf helm*",
-            "mv linux-amd64/helm /usr/local/bin/helm"
             "kubeadm token create --print-join-command",
+            "cp /root/kube-flannel.yml /home/ubuntu/",
+            "chown ubuntu: /home/ubuntu/kube-flannel.yml"
+            "sudo su - ubuntu"
+            "kubectl apply -f /home/ubuntu/kube-flannel.yml",
         )
 
         c1_cp1 = ec2.Instance(self, "ControlNode",
@@ -103,8 +106,7 @@ class WorkspaceStack(Stack):
 
         c1_node1_user_data = ec2.UserData.for_linux()
         c1_node1_user_data.add_commands(
-            "hostname c1-node1",
-            "echo 'c1-node1 > /etc/hostname'",
+            "hostnamectl hostname c1-node1",
             "apt update -y",
             "apt upgrade -y",
         )
@@ -122,23 +124,23 @@ class WorkspaceStack(Stack):
             key_pair=keyPair,
         )
 
-        c1_node2_user_data = ec2.UserData.for_linux()
-        c1_node2_user_data.add_commands(
-            "hostname c1-node2",
-            "echo 'c1-node2 > /etc/hostname'",
-            "apt update -y",
-            "apt upgrade -y",
-        )
+        # c1_node2_user_data = ec2.UserData.for_linux()
+        # c1_node2_user_data.add_commands(
+        #     "hostname c1-node2",
+        #     "echo 'c1-node2 > /etc/hostname'",
+        #     "apt update -y",
+        #     "apt upgrade -y",
+        # )
 
-        c1_node2 = ec2.Instance(self, "WorkerNode2",
-            vpc=vpc,
-            vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
-            instance_name="c1-node2",
-            instance_type=ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
-            machine_image=ec2.MachineImage.generic_linux({"us-east-2": ami_id}),
-            security_group=worker_node_sg,
-            role=ec2_role,
-            user_data=c1_node2_user_data,
-            user_data_causes_replacement=True,
-            key_pair=keyPair,
-        )
+        # c1_node2 = ec2.Instance(self, "WorkerNode2",
+        #     vpc=vpc,
+        #     vpc_subnets=ec2.SubnetSelection(subnet_type=ec2.SubnetType.PUBLIC),
+        #     instance_name="c1-node2",
+        #     instance_type=ec2.InstanceType.of(ec2.InstanceClass.T2, ec2.InstanceSize.MICRO),
+        #     machine_image=ec2.MachineImage.generic_linux({"us-east-2": ami_id}),
+        #     security_group=worker_node_sg,
+        #     role=ec2_role,
+        #     user_data=c1_node2_user_data,
+        #     user_data_causes_replacement=True,
+        #     key_pair=keyPair,
+        # )
